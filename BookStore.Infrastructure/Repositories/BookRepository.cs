77ef3a60fbc4ae889a973a1e47eb3016ea132a2f
alexkondrw <69770483+MikKut@users.noreply.github.com>
2024-07-17 11:41:10 +0300

    Changed responses/requests

--- BookStore.Infrastructure/Repositories/BookRepository.cs
+++ BookStore.Infrastructure/Repositories/BookRepository.cs
@@ -1,10 +1,13 @@
-﻿using BookStore.Domain.Entities;
+﻿using AutoMapper;
+using BookStore.Domain.Entities;
 using BookStore.Infrastructure.Data;
 using BookStore.Infrastructure.Requests;
+using BookStore.Infrastructure.Responses;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,14 +18,15 @@ namespace BookStore.Infrastructure.Repositories
     {
         private readonly ApplicationDbContext _context;
         private readonly ILogger<BookRepository> _logger;
-
-        public BookRepository(ApplicationDbContext context, ILogger<BookRepository> logger)
+        private readonly IMapper _mapper;
+        public BookRepository(ApplicationDbContext context, ILogger<BookRepository> logger, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
             _logger = logger;
         }
 
-        public async Task<IEnumerable<BookEntity>> GetAllBooksAsync(BooksPagedFilterRequest request, CancellationToken cancellationToken)
+        public async Task<PagedResult<BookEntity>> GetAllBooksAsync(BooksPagedFilterRequest request, CancellationToken cancellationToken)
         {
             try
             {
@@ -33,20 +37,36 @@ namespace BookStore.Infrastructure.Repositories
                     query = query.Where(b => b.Title.Contains(request.Name));
                 }
 
-                if (!string.IsNullOrEmpty(request.Date))
+                if (!string.IsNullOrEmpty(request.StartDate))
                 {
-                    if (DateTime.TryParse(request.Date, out var date))
+                    if (DateTime.TryParse(request.StartDate,  out var date))
                     {
-                        query = query.Where(b => b.PublicationDate == date);
+                        query = query.Where(b => b.PublicationDate >= date);
                     }
                 }
 
-                return await query.ToListAsync(cancellationToken);
+                if (!string.IsNullOrEmpty(request.EndDate))
+                {
+                    if (DateTime.TryParse(request.EndDate,  out var date))
+                    {
+                        query = query.Where(b => b.PublicationDate <= date);
+                    }
+                }
+
+                var totalCount = await query.CountAsync(cancellationToken);
+
+                var books = await query
+                    .Skip((request.PageNumber - 1) * request.PageSize)
+                    .Take(request.PageSize)
+                    .ToListAsync(cancellationToken);
+
+
+                return new PagedResult<BookEntity> {Items = books, TotalCount = totalCount };
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error in GetAllBooksAsync");
-                throw; // Re-throw the exception to propagate it to the caller
+                throw;
             }
         }
 
@@ -82,15 +102,19 @@ namespace BookStore.Infrastructure.Repositories
         {
             try
             {
-                var innerBook = await GetBookByIdAsync(book.Id, cancellationToken);
-                if (innerBook == null)
+                var existingBook = await _context.Books.FindAsync(new object[] { book.Id }, cancellationToken);
+                if (existingBook == null)
                 {
                     return null;
                 }
 
-                _context.Entry(book).State = EntityState.Modified;
+                existingBook.Title = book.Title;
+                existingBook.PublicationDate = book.PublicationDate;
+                existingBook.Description = book.Description;
+                existingBook.NumberOfPages = book.NumberOfPages;
+
                 await _context.SaveChangesAsync(cancellationToken);
-                return book;
+                return existingBook;
             }
             catch (Exception ex)
             {
--- BookStore.Infrastructure/Requests/BooksPagedFilterRequest.cs
+++ BookStore.Infrastructure/Requests/BooksPagedFilterRequest.cs
@@ -8,7 +8,10 @@ namespace BookStore.Infrastructure.Requests
 {
     public class BooksPagedFilterRequest
     {
-        public string? Date { get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
         public string? Name { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
     }
 }
